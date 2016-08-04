ab2e736bea1515446652063870c6fb5d89a53b16
19TT94 <taylor@pxlbros.com>
2016-08-03 18:18:16 -0700

    added routes

--- app/router.js
+++ app/router.js
@@ -7,6 +7,27 @@ const Router = Ember.Router.extend({
 
 Router.map(function() {
   this.route('index', {path: '/'});
+  this.route('contact');
+  this.route('team');
+  this.route('club');
+  this.route('sponsers');
 });
 
 export default Router;
+
+Ember.Route.reopen({
+    activate: function() {
+      var cssClass = this.toCssClass();
+      // you probably don't need the application class
+      // to be added to the body
+      if (cssClass != 'application') {
+        Ember.$('body').addClass(cssClass);
+      }
+    },
+    deactivate: function() {
+      Ember.$('body').removeClass(this.toCssClass());
+    },
+    toCssClass: function() {
+      return this.routeName.replace(/\./g, '-').dasherize();
+    }
+})
