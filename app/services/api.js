75fe161278e3590dda2850dd3c0e2521b04a0fe2
Taylor Tobin <19tt94@gmail.com>
2017-07-15 10:08:45 -0700

    setting up video modal

--- app/services/api.js
+++ app/services/api.js
@@ -1,22 +1,40 @@
 import Ember from 'ember';
 
+const modal = [
+    {
+        'title':'Nicaragua Team Trip 2017',
+        'url':''
+    }
+];
+
+const team = [
+    {
+        'name': 'Sebastian Ospina',
+        'position': 'President'
+    },
+    {
+        'name': 'Taylor Tobin',
+        'position': 'Vice President'
+    },
+    {
+        'name': 'Ian Tachel',
+        'position': 'Former President'
+    },
+    {
+        'name': 'Randy Harp',
+        'position': 'Former Vice President'
+    }
+];
+
+const reason = 'There was an error.';
+
 export default Ember.Service.extend({
-    // const team = [
-    //     {
-    //         'name': 'Sebastian Ospina',
-    //         'position': 'President'
-    //     },
-    //     {
-    //         'name': 'Taylor Tobin',
-    //         'position': 'Vice President'
-    //     },
-    //     {
-    //         'name': 'Ian Tachel',
-    //         'position': 'Former President'
-    //     },
-    //     {
-    //         'name': 'Randy Harp',
-    //         'position': 'Former Vice President'
-    //     }
-    // ]
+    getModal() {
+        return new Ember.RSVP.Promise((res, rej) => {
+            // on sucess
+            res(modal);
+            //on failure
+            rej(reason);
+        });
+    }
 });
