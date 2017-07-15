6648a8ee2eb3d6d8eb5ff2360505439ac4aa5172
Taylor Tobin <19tt94@gmail.com>
2017-07-14 19:55:05 -0700

    added profile grid component

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+    // api: Ember.inject.service(),
+
+    // return this.get('api');
 });
---
+++ app/services/api.js
@@ -0,0 +1,22 @@
+import Ember from 'ember';
+
+export default Ember.Service.extend({
+    // const team = [
+    //     {
+    //         'name': 'Sebastian Ospina',
+    //         'position': 'President'
+    //     },
+    //     {
+    //         'name': 'Taylor Tobin',
+    //         'position': 'Vice President'
+    //     },
+    //     {
+    //         'name': 'Ian Tachel',
+    //         'position': 'Former President'
+    //     },
+    //     {
+    //         'name': 'Randy Harp',
+    //         'position': 'Former Vice President'
+    //     }
+    // ]
+});
